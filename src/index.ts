e2d294cf84dea76ae3f2891f57e95223b2e6cc73
Varkichan Cheeramkuzhiyil <varcheer@publicisgroupe.net>
2023-11-14 17:37:34 +0530

    feat: add an option for taking prefix inputs, predefine styles for prefixes based on log level, add jsdocs, add time taken calculator for async tasks

--- src/index.ts
+++ src/index.ts
@@ -65,48 +65,81 @@ function stylizeText(msg: string, config?: TStylizeTextConfig): string {
 function statusSuccess(
   msg: string,
   config?: TStatusMsgConfig | TStylizeTextConfig,
+  prefix?: string,
 ): string {
-  return stylizeText(msg, { ...config, color: 'greenBright' });
+  const prefixTxt = stylizeText(` ${prefix} `, {
+    bgColor: 'bgGreenBright',
+    modifier: 'bold',
+  });
+  return stylizeText(`${prefix ? `> ${prefixTxt}` : ''} ${msg}`, {
+    ...config,
+    color: 'greenBright',
+  });
 }
 
 function statusError(
   msg: string,
   config?: TStatusMsgConfig | TStylizeTextConfig,
+  prefix?: string,
 ): string {
-  return stylizeText(msg, { ...config, color: 'redBright' });
+  const prefixTxt = stylizeText(` ${prefix} `, {
+    bgColor: 'bgRedBright',
+    modifier: 'bold',
+  });
+  return stylizeText(`${prefix ? `> ${prefixTxt}` : ''} ${msg}`, {
+    ...config,
+    color: 'redBright',
+  });
 }
 
 function statusInfo(
   msg: string,
   config?: TStatusMsgConfig | TStylizeTextConfig,
+  prefix?: string,
 ): string {
-  return stylizeText(msg, { ...config, color: 'blueBright' });
+  const prefixTxt = stylizeText(` ${prefix} `, {
+    bgColor: 'bgBlueBright',
+    modifier: 'bold',
+  });
+  return stylizeText(`${prefix ? `> ${prefixTxt}` : ''} ${msg}`, {
+    ...config,
+    color: 'blueBright',
+  });
 }
 
 function statusWarn(
   msg: string,
   config?: TStatusMsgConfig | TStylizeTextConfig,
+  prefix?: string,
 ): string {
-  return stylizeText(msg, { ...config, color: 'yellowBright' });
+  const prefixTxt = stylizeText(` ${prefix} `, {
+    bgColor: 'bgYellowBright',
+    modifier: 'bold',
+  });
+  return stylizeText(`${prefix ? `> ${prefixTxt}` : ''} ${msg}`, {
+    ...config,
+    color: 'yellowBright',
+  });
 }
 
 function statusMsg(
   msg: string,
   config?: TStatusMsgConfig | TStylizeTextConfig,
   status?: TStatus,
+  prefix?: string,
 ) {
   switch (status) {
     case 'warn':
-      console.warn(statusWarn(msg, config));
+      console.warn(statusWarn(msg, config, prefix));
       break;
     case 'error':
-      console.warn(statusError(msg, config));
+      console.warn(statusError(msg, config, prefix));
       break;
     case 'info':
-      console.warn(statusInfo(msg, config));
+      console.warn(statusInfo(msg, config, prefix));
       break;
     case 'success':
-      console.log(statusSuccess(msg, config));
+      console.log(statusSuccess(msg, config, prefix));
       break;
     default:
       console.log(stylizeText(msg, config));
@@ -115,59 +148,116 @@ function statusMsg(
 }
 
 const g = gradient;
+let start: [number, number], end: [number, number];
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export default class sa {
+  /**
+   * @description log to console
+   * @param {string} msg text to be logged
+   * @param {TStylizeTextConfig} config configuration to stylize the text
+   */
   static log(msg: string, config?: TStylizeTextConfig): void {
     statusMsg(msg, config);
   }
 
-  static warn(msg: string, config?: TStatusMsgConfig): void {
-    statusMsg(msg, config, 'warn');
+  /**
+   * @description log prestyled warning to console
+   * @param {string} msg text to be logged
+   * @param {TStylizeTextConfig} config configuration to stylize the text
+   * @param {string} prefix prestyled prefix to be added (eg. brand name)
+   */
+  static warn(msg: string, config?: TStatusMsgConfig, prefix?: string): void {
+    statusMsg(msg, config, 'warn', prefix || '');
   }
 
-  static error(msg: string, config?: TStatusMsgConfig): void {
-    statusMsg(msg, config, 'error');
+  /**
+   * @description log prestyled error message to console
+   * @param {string} msg text to be logged
+   * @param {TStylizeTextConfig} config configuration to stylize the text
+   * @param {string} prefix prestyled prefix to be added (eg. brand name)
+   */
+  static error(msg: string, config?: TStatusMsgConfig, prefix?: string): void {
+    statusMsg(msg, config, 'error', prefix || '');
   }
 
-  static success(msg: string, config?: TStatusMsgConfig): void {
-    statusMsg(msg, config, 'success');
+  /**
+   * @description log prestyled success message to console
+   * @param {string} msg text to be logged
+   * @param {TStylizeTextConfig} config configuration to stylize the text
+   * @param {string} prefix prestyled prefix to be added (eg. brand name)
+   */
+  static success(
+    msg: string,
+    config?: TStatusMsgConfig,
+    prefix?: string,
+  ): void {
+    statusMsg(msg, config, 'success', prefix || '');
   }
 
-  static info(msg: string, config?: TStatusMsgConfig): void {
-    statusMsg(msg, config, 'info');
+  /**
+   * @description log prestyled info to console
+   * @param {string} msg text to be logged
+   * @param {TStylizeTextConfig} config configuration to stylize the text
+   * @param {string} prefix prestyled prefix to be added (eg. brand name)
+   */
+  static info(msg: string, config?: TStatusMsgConfig, prefix?: string): void {
+    statusMsg(msg, config, 'info', prefix || '');
   }
 
-  static start(msg: string, config?: TStylizeTextConfig): Ora {
-    return ora(stylizeText(msg, config)).start();
+  /**
+   * @description to start ora loader (spinner)
+   * @param {string} msg text to be logged while starting the spinner
+   * @param {TStylizeTextConfig} config configuration to stylize the text
+   * @returns {Ora}
+   */
+  static start(msg?: string, config?: TStylizeTextConfig): Ora {
+    start = process.hrtime();
+    return ora(stylizeText(msg || '', config)).start();
   }
 
+  /**
+   * @description to stop ora loader (spinner)
+   * @param {Ora} ora reference to an already created spinner
+   * @param {string} msg text to be logged while stopping the spinner
+   * @param {TStylizeTextConfig} config configuration to stylize the text
+   */
   static stop(
     ora: Ora,
-    msg: string,
+    msg?: string,
     config?: TStylizeTextConfig,
     status?: TStatus,
   ): void {
-    const txt = stylizeText(msg, config);
+    const txt = stylizeText(msg || '', config);
+    end = process.hrtime(start);
+    const elapsedTime = (end[0] * 1e9 + end[1]) / 1e6;
+    const seconds = Math.ceil(elapsedTime / 1000);
+
     switch (status) {
       case 'warn':
-        ora.warn(statusWarn(txt));
+        ora.warn(statusWarn(`${txt} (${seconds}s)`));
         break;
       case 'info':
-        ora.info(statusInfo(txt));
+        ora.info(statusInfo(`${txt} (${seconds}s)`));
         break;
       case 'success':
-        ora.succeed(statusSuccess(txt));
+        ora.succeed(statusSuccess(`${txt} (${seconds}s)`));
         break;
       case 'error':
-        ora.fail(statusError(txt));
+        ora.fail(statusError(`${txt} (${seconds}s)`));
         break;
       default:
-        ora.stopAndPersist({ text: txt });
+        ora.stopAndPersist({ text: `${txt} (${seconds}s)` });
         break;
     }
     ora.stop();
   }
 
+  /**
+   * @description to animate the text with prebuilt animations
+   * @param {string} msg text to be logged
+   * @param {IShellArtistConfig['animation']} animation prebuilt animations
+   * @returns {Animation}
+   */
   static animate(
     msg: string,
     animation: IShellArtistConfig['animation'],
@@ -178,6 +268,12 @@ export default class sa {
     return chalkAnimation.glitch(msg);
   }
 
+  /**
+   * @description to apply gradients on the text
+   * @param {string} msg text to be logged
+   * @param {IShellArtistConfig['gradient']} gradient prebuilt gradients or custom array of colors
+   * @returns {string}
+   */
   static applyGradient(
     msg: string,
     gradient: IShellArtistConfig['gradient'],
@@ -192,6 +288,12 @@ export default class sa {
     return msg;
   }
 
+  /**
+   * @description to create ascii art
+   * @param {string} msg text to be logged
+   * @param {IShellArtistConfig['ascii']} ascii configuration for creating custom ascii arts
+   * @returns {string}
+   */
   static createAscii(msg: string, ascii?: IShellArtistConfig['ascii']): string {
     const asciiTxt = figlet.textSync(msg, {
       font: 'Standard' ?? ascii?.font,
